b1d72d426c5f47c9a7451819c03d2310d644b20f
dudagomessk <eduardagomesnovo@gmail.com>
2026-04-24 15:01:53 -0300

    first commit

---
+++ src/components/ProdutoChild.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ButtonChild from './icons/ButtonChild.vue';
+defineProps(['id', 'nome', 'preco', 'categoria'])
+
+function teste(){
+  alert('OPa')
+}
+</script>
+
+<template>
+  <li>
+    ID: {{ id }} - {{ nome }} -- R$ {{ preco }} ({{ categoria }})
+    <ButtonChild @clique="teste">Corrigir Preço</ButtonChild>
+  </li>
+</template>
+
+<style scoped></style>
---
+++ src/components/icons/ButtonChild.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineEmits(['clique'])
+</script>
+<template>
+  <button @click.prevent="$emit('clique')">
+<slot>
+  
+</slot>
+  </button>
+</template>
+<style scoped></style>
+
