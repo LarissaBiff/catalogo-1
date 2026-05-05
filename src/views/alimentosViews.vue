632d5102a83d5e67ee66bba97d513c9f7fa5a515
dudagomessk <eduardagomesnovo@gmail.com>
2026-05-05 11:57:11 -0300

    okk

---
+++ src/views/alimentosViews.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import { ref } from 'vue';
+import { listaProdutos } from '@/utils/produtos';
+const produtos = ref(listaProdutos)
+produtos.value = produtos.value.filter(produto => produto.categoria === 'Alimentos')
+
+
+</script>
+
+<template>
+
+
+</template>
+
+
+<style scoped>
+
+</style>
