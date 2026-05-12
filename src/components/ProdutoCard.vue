384b3d074f3005aa45c080b3059f8edf40d085b9
LarissaBiff <biff.larissa@gmail.com>
2026-05-12 10:59:19 -0300

    ...

---
+++ src/components/ProdutoCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+    defineProps((['id', 'nome', 'preco', 'imagem', 'categoria']))
+    import ButtonChild from './ButtonChild.vue';
+    import { formataPreco } from '@/utils/produtosUtils';
+</script>
+<template>
+    <div class="produto-card">
+        <div>
+            <h2>
+                {{ nome }}
+            </h2>
+            <p>
+                Preço: {{ formataPreco(preco) }}
+            </p>
+        </div>
+        <div>
+            <img :src="imagem" :alt="nome">
+        </div>
+    </div>
+</template>
+<style scoped>
+
+</style>
